ed344fa5e584fd7d9a21f32d7cc225364c4fd937
Rodrigo Duarte <rodrigoduartesilva@yahoo.com.br>
2024-05-09 12:20:33 -0300

    feat: edicao do campo de softskills no profile.json

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,30 @@
+function updateProfileInfo(profileData) {
+    const photo = document.getElementById('profile.photo');
+    const name = document.getElementById('profile.name');
+    const job = document.getElementById('profile.job');
+    const location = document.getElementById('profile.location');
+    const phone = document.getElementById('profile.phone');
+    const email = document.getElementById('profile.email');
+
+
+    photo.src = profileData.photo;
+    photo.alt = profileData.name
+
+    name.innerText = profileData.name;
+
+    job.innerText = profileData.job;
+
+    location.innerText = profileData.location.city;
+
+    phone.innerText = profileData.phone;
+    phone.href = `tel:${profileData.phone}`;
+
+    email.innerText = profileData.email;
+    email.href = `mailto:${profileData.email}`;
+
+}
+
 (async () => {
     const profileData = await fetchProfileData();
-    console.log(profileData);
+    updateProfileInfo(profileData);
 })();
