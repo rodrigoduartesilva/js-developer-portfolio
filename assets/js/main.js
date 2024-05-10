546d8c0371e9b1f6b12e8a3d8074749ee0fec3a1
Rodrigo Duarte <rodrigoduartesilva@yahoo.com.br>
2024-05-10 18:34:28 -0300

    fix: modificacao dos icones das hasdskills.

--- assets/js/main.js
+++ assets/js/main.js
@@ -34,8 +34,6 @@ function updateHardSkills(profileData) {
     const hardSkills = document.getElementById('profile.skills.hardSkills');
 
     hardSkills.innerHTML = profileData.skills.hardSkills.map((skill) => `<li><img src="${skill.logo}" alt="${skill.name}" title="${skill.name}"></li>`).join('');
-
-    console.log(hardSkills);
 }
 
 
