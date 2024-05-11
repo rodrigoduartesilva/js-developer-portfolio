ff0a2f0017cb110bab8dd4a4fb4ccf9980d23213
Rodrigo Duarte <rodrigoduartesilva@yahoo.com.br>
2024-05-10 23:20:25 -0300

    feat: portfolio inserido.

--- assets/js/main.js
+++ assets/js/main.js
@@ -36,6 +36,26 @@ function updateHardSkills(profileData) {
     hardSkills.innerHTML = profileData.skills.hardSkills.map((skill) => `<li><img src="${skill.logo}" alt="${skill.name}" title="${skill.name}"></li>`).join('');
 }
 
+function updateLanguages(profileData) {
+    const languages = document.getElementById('profile.languages');
+
+    languages.innerHTML = profileData.languages.map((language) => `<li>${language}</li>`).join('');
+
+    console.log(languages);
+}
+
+function updatePortfolio(profileData) {
+    const portfolio = document.getElementById('profile.portfolio');
+
+    portfolio.innerHTML = profileData.portfolio.map((project) => {
+        return `
+            <li>
+                <h3 ${project.github ? 'class="github"' : ''}>${project.name}</h3>
+                <a href="${project.url}" target="_blank">${project.url}</a>
+            </li>
+        `
+    }).join('');
+}
 
 
 
@@ -44,4 +64,6 @@ function updateHardSkills(profileData) {
     updateProfileInfo(profileData);
     updateSoftSkills(profileData);
     updateHardSkills(profileData);
+    updateLanguages(profileData);
+    updatePortfolio(profileData);
 })();
